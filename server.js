572812a7284a8a2be4fb8e651a606140ec525628
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-06-04 11:03:48 -0400

    added session feature to server.js

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3001;
+const session = require('express-session');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("client/build"));
+
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: false,
+};
+  
+app.use(session(sess));
 
 app.use(routes);
 
