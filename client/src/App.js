4b2141d95a998381bce290e14d0e5c4b4b183f97
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-06-04 23:05:47 -0400

    created frontend routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Route, Switch } from 'react-router-dom';
+
+import AllCharactersPage from './pages/allCharacters';
+import NewCharacterPage from './pages/newCharacter';
+import MyCharactersPage from './pages/myCharacters';
+import LoginPage from './pages/login';
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <Switch>
+        <Route path='/' exact>
+          <AllCharactersPage />
+        </Route>
+        <Route path='/newCharacter'>
+          <NewCharacterPage />
+        </Route>
+        <Route path='/myCharacters'>
+          <MyCharactersPage />
+        </Route>
+        <Route path='/login'>
+          <LoginPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 
 export default App;
+
+//Logo Code
+//<img src={logo} className="App-logo" alt="logo" />
+
+//Background Code
+//<img src={OldPaper} className="Background" alt="OldPaper" />
---
+++ client/src/pages/newCharacter.js
@@ -0,0 +1,5 @@
+function NewCharacterPage() {
+    return <div></div>
+}
+
+export default NewCharacterPage;
